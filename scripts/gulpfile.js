55e97e3170a7bb13854512d6367bef82a859284f
MengHsiu Chang <shou@MengHsius-MacBook-Pro.local>
2016-07-13 10:53:53 +0800

    Remove unnecessary files

--- scripts/gulpfile.js
+++ scripts/gulpfile.js
@@ -7,7 +7,7 @@ var util = require('util');
 var requirePostfixUpdator = require('gulp-ojbridge').requirePostfixModifier;
 var encrypt = require('gulp-ojbridge').encrypt;
 
-const EXAMPLE_APP_PATH = '../OJBDevicePlayground';
+// const EXAMPLE_APP_PATH = '../OJBDevicePlayground';
 const POD_SOURCE_PATH = 'encrypted';
 const PASSWORD = '123fartasvfdfhasrtasef';
 const TS_DEFINITIONS = './release/definitions'
@@ -44,12 +44,12 @@ gulp.task('watch', function() {
         
         console.log('working on: ' + sourceName);
         return gulp.src(event.path, {})
-        .pipe(gulp.dest(EXAMPLE_APP_PATH))
+        // .pipe(gulp.dest(EXAMPLE_APP_PATH))
         .pipe(requirePostfixUpdator('es'))
         .pipe(encrypt(PASSWORD))
         .pipe(dest('./', {ext: 'es'}))
-        .pipe(gulp.dest(POD_SOURCE_PATH))
-        .pipe(gulp.dest(EXAMPLE_APP_PATH));
+        .pipe(gulp.dest(POD_SOURCE_PATH));
+        // .pipe(gulp.dest(EXAMPLE_APP_PATH));
     });
 });
 
